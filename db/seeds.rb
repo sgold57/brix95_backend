3cafaf74261687f22ead31bd14ca37b0d91dd146
Sam Gold <samg915757@gmail.com>
2021-07-09 12:43:33 -0600

    more seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,3 +28,54 @@ Strain.create({
   species: "hybrid",
   description: "This strain would be delicious, except it's not because it's laced with bug spray."
 })
+
+Strain.create({
+  name: "Cookie Crumbs",
+  thc_content: 5.43,
+  species: "indica",
+  description: "This strain is exclusively shake, sourced from the couches of prominent stoners in California. No seeds, no stems, but, if you're lucky, a little bit of lint."
+})
+
+Strain.create({
+  name: "Skunk In The Trunk",
+  thc_content: 19.82,
+  species: "sativa",
+  description: "Not advised for young potheads living at home with their parents; the aroma of this strain eludes double bagging, triple bagging, ten bags. IT REEKS."
+})
+
+Strain.create({
+  name: "Oregano",
+  thc_content: 0.00,
+  species: "spice",
+  description: "Gotcha bitch! When is this stuff going to be legal?"
+})
+
+Strain.create({
+  name: "Scottie Rippin",
+  thc_content: 23.23,
+  species: "hybrid",
+  description: "Potency is MJ's number twice, and the purple color looks like Rodman's hair. Misnamed IMHO, but I don't make the rules."
+})
+
+Strain.create({
+  name: "Forrest Hemp",
+  thc_content: 19.95,
+  species: "sativa",
+  description: "Named after the Best Picture Winner of this year, this strain may make you a little slow, or compel you to run cross country." 
+
+})
+
+Strain.create({
+  name: "Central Purp",
+  thc_content: 22.87,
+  species: "hybrid",
+  description: "Need a break, like Ross? Why do you think Gunther is so chill all the time?"
+})
+
+Strain.create({
+  name: "The OG",
+  thc_content: 1.43,
+  species: "indica",
+  description: "Super rare, this is actually the first strain discovered. Dry and stale, but a great story to email your friends on AOL version 4.5!"
+})
+
